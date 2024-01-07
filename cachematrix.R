f6452265032b41a6a4ea5debb2578b4e30dcd489
Sonya0122 <chenlu_0122@outlook.com>
2024-01-08 01:53:39 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,28 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##There are two functions makeCacheMatrix,library(MASS)
+##makeCacheMatrix consists of set,get,setinv,getinv
+##library(MASS) is used for calculating matrix
+
+library(MASS)
+makeCacheMatrix<-function(x=matrix()){
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<NULL
+  }
+  get<-function(x)
+  setinv<-function(inverse)inv<<-inverse
+  getinv<-function(){
+    inver<-ginv(x)
+    inver%*%x
+  }
+  list(set=set,get=get,
+       setinv=setinv
+       getinv=getinv)
+}
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -9,7 +31,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+##This is used to get the cached data
 cacheSolve <- function(x, ...) {
+  inv<-x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinv(inv)
+  inv
         ## Return a matrix that is the inverse of 'x'
 }
+> f<-makeCacheMatrix(matrix(1:8,2,4))
+> f$get()
+
